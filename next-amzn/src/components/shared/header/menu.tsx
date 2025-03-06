1dc3dfa37dc0d4fc1b668babdadee4a74f3c99ed
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-03-06 20:38:37 +0530

    created website layout

---
+++ next-amzn/src/components/shared/header/menu.tsx
@@ -0,0 +1,19 @@
+import { ShoppingCartIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export default function Menu() {
+  return (
+    <div className='flex justify-end'>
+      <nav className='flex gap-3 w-full'>
+        <Link href='/cart' className='flex items-center header-button'>
+          {/* <UserIcon className='h-8 w-8' /> */}
+          Hello, Sign in
+        </Link>
+        <Link href='/cart' className='header-button'>
+          <ShoppingCartIcon className='h-8 w-8' />
+          Cart
+        </Link>
+      </nav>
+    </div>
+  )
+}
---
+++ next-amzn/src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NextAmazon'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Never give up'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Next.js + Tailwind CSS + TypeScript + Shadcn UI'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
