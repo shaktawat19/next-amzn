9d490cb8f502295c0cf5281761e54fe1e79b223c
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-03-06 22:52:23 +0530

    setting up mongodb

--- next-amzn/src/lib/constants.ts
+++ next-amzn/src/lib/constants.ts
@@ -3,6 +3,7 @@ export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Never give up'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'Next.js + Tailwind CSS + TypeScript + Shadcn UI'
+export const MONGODB_URI = process.env.MONGODB_URI
 
 export const USER_ROLES = ['Admin', 'User']
 export const COLORS = ['Gold', 'Green', 'Red']
